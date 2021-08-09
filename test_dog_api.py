e17a1d14b1e1cd7afefe99c4ec45d4d32bb6b22c
prusova_y <y-prusova@gmail.com>
2021-08-09 11:47:48 +0300

    Change: test_dog_api:test_random_image -> test_dog_api:test_image (with parametrize)

--- test_dog_api.py
+++ test_dog_api.py
@@ -1,5 +1,6 @@
 from help import check_get
 from jsonschema import validate
+import pytest
 
 
 MESSAGE = "message"
@@ -8,7 +9,13 @@ STATUS = "status"
 STATUS_OK = 200
 
 
-def test_random_image():
+@pytest.mark.parametrize("url",
+                         [
+                             pytest.param("https://dog.ceo/api/breeds/image/random", id="random_image"),
+                             pytest.param("https://dog.ceo/api/breed/hound/images/random",
+                                          id="random_image_from_a_breed_collection")
+                         ])
+def test_image(url: str):
     CONTENT_SCHEMA = {
         "type": "object",
         "properties": {
@@ -17,9 +24,12 @@ def test_random_image():
         }
     }
     EXPECTED_STATUS = "success"
-    response = check_get("https://dog.ceo/api/breeds/image/random", STATUS_OK)
+    response = check_get(url, STATUS_OK)
     response_json = response.json()
     validate(response_json, CONTENT_SCHEMA)
     assert response_json.get(STATUS) == EXPECTED_STATUS, f"Unexpected status: \"{response_json.get(STATUS)}\". " \
                                                          f"\"{EXPECTED_STATUS}\" is expected"
     check_get(response_json.get(MESSAGE), STATUS_OK)
+
+
+
